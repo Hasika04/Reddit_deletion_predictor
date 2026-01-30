ee53e8642b9c23cd27fce66ab1dff4d72d60d823
Hasika Penna <Hasika04_hasika.penna.che22@itbhu.ac.in>
2026-01-31 02:08:59 +0530

    Reddit API

--- app/main.py
+++ app/main.py
@@ -1,11 +1,30 @@
+import pickle
+import os
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def get_server():
-    return {"message":"Server Live!"}
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-@app.get("/name")
-def get_name():
-    return {"name":"fastapi"}
+model_path = os.path.join(BASE_DIR, "model.pkl")
+tfidf_path = os.path.join(BASE_DIR, "vectorizer.pkl")
+
+with open("C:/Users/LENOVO/Downloads/app/vectorizer.pkl", "rb") as f:
+    tfidf = pickle.load(f)
+
+with open("C:/Users/LENOVO/Downloads/app/model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+class Post(BaseModel):
+    title: str
+
+@app.post("/predict")
+def predict(post: Post):
+    text_vector = tfidf.transform([post.title])
+    prediction = model.predict(text_vector)
+    return {"prediction": int(prediction[0])}
+
+@app.get("/")
+def read_root():
+    return {"message": "My Reddit App is running!"}
